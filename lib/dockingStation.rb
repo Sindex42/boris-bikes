e1eae5eb162d1f80b364b90f1e3f9934b19d368b
Connor Kam-Cheong <connorkc42@gmail.com>
2018-11-29 15:48:13 +0000

    Added max_capacity to DockingStation, docking increments bikes

--- lib/dockingStation.rb
+++ lib/dockingStation.rb
@@ -3,12 +3,22 @@ require_relative 'bike'
 class DockingStation
 
   attr_reader :bike
-  
+  attr_accessor :capacity
+
+  def initialize
+    @capacity = 0
+    @max_capacity = 20
+  end
+
   def release_bike
+    raise 'Not enough bikes' if @capacity == 0
+    @capacity -= 1
     Bike.new
   end
 
   def dock(bike)
+    raise 'At capacity!' if @capacity == @max_capacity
+    @capacity += 1
     @bike = bike
   end
 
--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -1,28 +1,44 @@
 require 'dockingStation'
 
 describe DockingStation do
+  let(:bike) {Bike.new}
 
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
 
   it 'releases a working bike' do
-    bike = subject.release_bike
+    subject.capacity = 1
+    released_bike = subject.release_bike
 
-    expect(bike).to be_working
+    expect(released_bike).to be_working
   end
 
   it 'returns a bike to the docking station' do
-    bike = Bike.new
-
     expect(subject.dock(bike)).to eq bike
   end
 
   it 'shows if a bike is docked' do
-    bike = Bike.new
     subject.dock(bike)
 
     expect(subject.bike).to eq bike
   end
 
+  it 'does not release a bike if there are no bikes' do
+    subject.capacity = 0
+    expect { subject.release_bike }.to raise_error('Not enough bikes')
+  end
+
+  it 'does not dock a bike if capacity is full' do
+    subject.capacity = 20
+    expect { subject.dock(bike) }.to raise_error('At capacity!')
+  end
+
+  it 'after docking 20 times, should reach full capacity of 20' do
+    # set up a docking station
+    # checking 20 tiimes of docking a bike results in a capcity of 20
+    station = DockingStation.new
+    20.times { station.dock Bike.new }
+    expect(station.capacity).to be(20)
+  end
 end
