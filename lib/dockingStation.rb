0d924d0b9ef246d99342f741dea58013a9d054b6
Connor Kam-Cheong <connorkc42@gmail.com>
2018-11-29 17:11:47 +0000

    Refactored dockingStation.rb

--- lib/dockingStation.rb
+++ lib/dockingStation.rb
@@ -3,24 +3,26 @@ require_relative 'bike'
 class DockingStation
 
   attr_reader :bikes
-  attr_accessor :capacity
 
   def initialize
     @bikes = []
-    @capacity = 0
     @max_capacity = 20
   end
 
   def release_bike
-    raise 'Not enough bikes' if @capacity == 0
-    @capacity -= 1
+    raise 'Not enough bikes' if @bikes.empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise 'At capacity!' if @capacity == @max_capacity
-    @capacity += 1
+    raise 'At capacity!' if @bikes.count == @max_capacity
     @bikes << bike
+  end
+
+  private
+
+  def full?
+
   end
 
 end
--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -8,7 +8,6 @@ describe DockingStation do
   it { is_expected.to respond_to(:bikes) }
 
   it 'releases a working bike' do
-    # subject.capacity = 1
     subject.dock(bike)
     released_bike = subject.release_bike
 
@@ -16,30 +15,29 @@ describe DockingStation do
   end
 
   it 'returns a bike to the docking station' do
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
   it 'shows if a bike is docked' do
     subject.dock(bike)
 
-    expect(subject.bikes).to eq bike
+    expect(subject.bikes).to eq [bike]
   end
 
   it 'does not release a bike if there are no bikes' do
-    subject.capacity = 0
+
     expect { subject.release_bike }.to raise_error('Not enough bikes')
   end
 
   it 'does not dock a bike if capacity is full' do
-    subject.capacity = 20
+    20.times { subject.dock Bike.new }
     expect { subject.dock(bike) }.to raise_error('At capacity!')
   end
 
   it 'after docking 20 times, should reach full capacity of 20' do
     # set up a docking station
     # checking 20 tiimes of docking a bike results in a capcity of 20
-    station = DockingStation.new
-    20.times { station.dock Bike.new }
-    expect(station.capacity).to be(20)
+    20.times { subject.dock Bike.new }
+    expect(subject.bikes.count).to be(20)
   end
 end
